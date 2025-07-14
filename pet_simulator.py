0e79779a4dfeed76c10eb3f47111477165b3eb45
Rafael-Ian <cabertesilorio@gmai.com>
2025-07-15 02:10:15 +0800

    pet_simulator_with_program_loop

---
+++ pet_simulator.py
@@ -0,0 +1,4 @@
+#Saves the pet's current data to a json file
+#Loads the pet's data from a file and return the correct pet type
+#Lets user pick the type of pet they want and choose a name for it
+#Main program loop
