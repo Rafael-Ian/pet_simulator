38e43f5190f6e93a2f4f9caf045184dd58112050
Rafael-Ian <cabertesilorio@gmai.com>
2025-07-14 22:46:55 +0800

    pets_functions

---
+++ pets.py
@@ -0,0 +1,14 @@
+#Base class for all pets
+#Person initializes a pet with name, type, and default stats
+#Feeds the pet function
+#Plays with the pet
+#Lets the pet rest and recover energy
+#Adds experience points
+#Displays the pet's current stats
+#Increase the pet's age and checks for evolution
+#Evolves the pet if age is 5 or above and not yet evolved
+#Evolves the pet, changes its species name, and boost health 
+#Apply time-based stat decay for realism
+#Converts pet's data to a dictionary for saving
+#Loads pet data from dictionary
+
