a8bf359a434a62a75bcb452864d8181a821fb170
Rafael-Ian <cabertesilorio@gmai.com>
2025-07-15 02:11:34 +0800

    imports

--- pet_simulator.py
+++ pet_simulator.py
@@ -1,3 +1,8 @@
+from pet import Dog, Cat, Bunny, Panda, Pet
+import json
+import os
+import time
+
 #Saves the pet's current data to a json file
 #Loads the pet's data from a file and return the correct pet type
 #Lets user pick the type of pet they want and choose a name for it
