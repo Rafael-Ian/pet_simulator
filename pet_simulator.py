794fcf475694dc75d3788167786a205698fa086a
Rafael-Ian <cabertesilorio@gmai.com>
2025-07-15 23:22:06 +0800

    loads_pets_data_from_file

--- pet_simulator.py
+++ pet_simulator.py
@@ -7,7 +7,24 @@ import time
 def save_pet(pet, filename="data.json"):
     with open(filename, "w") as f:
         json.dump(pet.to_dict(), f)
-        
+
 #Loads the pet's data from a file and return the correct pet type
+def load_pet(filename="data.json"):
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            data = json.load(f)
+            species = data["species"]
+            if "Dog" in species:
+                return Dog(**data)
+            elif "Cat" in species:
+                return Cat(**data)
+            elif "Bunny" in species:
+                return Bunny(**data)
+            elif "Panda" in species:
+                return Panda(**data)
+            else:
+                return Pet.from_dict(data)
+    return None
+
 #Lets user pick the type of pet they want and choose a name for it
 #Main program loop
