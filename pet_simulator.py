1d551f8e3366b842ab5c0c4dd09cd8594dd75dff
Rafael-Ian <cabertesilorio@gmai.com>
2025-07-15 23:17:49 +0800

    save_pets_data

--- pet_simulator.py
+++ pet_simulator.py
@@ -4,6 +4,10 @@ import os
 import time
 
 #Saves the pet's current data to a json file
+def save_pet(pet, filename="data.json"):
+    with open(filename, "w") as f:
+        json.dump(pet.to_dict(), f)
+        
 #Loads the pet's data from a file and return the correct pet type
 #Lets user pick the type of pet they want and choose a name for it
 #Main program loop
